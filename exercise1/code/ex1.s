87bd18452770c351fbc6130234471218e767cdee
Håkon Åmdal <hakon@aamdal.com>
2014-01-28 14:56:04 +0100

    Tar ut gpio_setup i egen fil for latexskriving

--- exercise1/code/ex1.s
+++ exercise1/code/ex1.s
@@ -82,37 +82,8 @@
 	      .type   _reset, %function
 		.thumb_func
 _reset: 
-	// load CMU base adress
-	ldr r1, cmu_base_addr
+	.include "gpio_setup.s"
 	
-	// Enable GPIO-clk
-	// load current value of HFPERCLK ENABLE
-	ldr r2, [r1, #CMU_HFPERCLKEN0]
-
-	// set bit for GPIO-clk
-	mov r3, #1
-	lsl r3, r3 , #CMU_HFPERCLKEN0_GPIO 
-	orr r2, r2, r3
-
-	// store new value
-	str r2, [r1,  #CMU_HFPERCLKEN0]
-
-	ldr r1, gpio_pa_base
-
-	mov r2, 0x2
-	str r2, [r1, #GPIO_CTRL]
-	
-	mov r2, 0x55555555
-	str r2, [r1, #GPIO_MODEH]
-	
-	ldr r3, gpio_pc_base
-	
-	mov r2, 0x33333333
-	str r2, [r3, #GPIO_MODEL]
-
-	mov r2, 0xff
-	str r2, [r3, #GPIO_DOUT]
-
 	b withoutinter
 
 withoutinter:
