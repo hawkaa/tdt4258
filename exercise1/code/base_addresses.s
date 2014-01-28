1a0e7aa65a62acaed3c3958c878a69dfa801730a
Håkon Åmdal <hakon@aamdal.com>
2014-01-28 15:18:41 +0100

    Skiller ut baseadresser i assemblyfil

---
+++ exercise1/code/base_addresses.s
@@ -0,0 +1,8 @@
+cmu_base_addr:
+	.long CMU_BASE
+
+gpio_pa_base:
+	.long GPIO_PA_BASE
+
+gpio_pc_base:
+	.long GPIO_PC_BASE
--- exercise1/code/ex1.s
+++ exercise1/code/ex1.s
@@ -118,11 +118,4 @@ gpio_handler:
 dummy_handler:  
         b .  // do nothing
 
-cmu_base_addr:
-	.long CMU_BASE
-
-gpio_pa_base:
-	.long GPIO_PA_BASE
-
-gpio_pc_base:
-	.long GPIO_PC_BASE
+.include "base_addresses.s"
