b793cea58f9e71dc694715cbe2d47bc558160966
Håkon Åmdal <hakon@aamdal.com>
2014-02-04 14:31:20 +0100

    legger til hawk sin interrupt-fil

---
+++ exercise1/code/interrupt.s
@@ -0,0 +1,107 @@
+	.syntax unified
+	.include "efm32gg.s"
+	.section .vectors
+	      .long   0x1000               /* Top of Stack                 */
+	      .long   _reset                  /* Reset Handler                */
+	      .long   dummy_handler           /* NMI Handler                  */
+	      .long   dummy_handler           /* Hard Fault Handler           */
+	      .long   dummy_handler           /* MPU Fault Handler            */
+	      .long   dummy_handler           /* Bus Fault Handler            */
+	      .long   dummy_handler           /* Usage Fault Handler          */
+	      .long   dummy_handler           /* Reserved                     */
+	      .long   dummy_handler           /* Reserved                     */
+	      .long   dummy_handler           /* Reserved                     */
+	      .long   dummy_handler           /* Reserved                     */
+	      .long   dummy_handler           /* SVCall Handler               */
+	      .long   dummy_handler           /* Debug Monitor Handler        */
+	      .long   dummy_handler           /* Reserved                     */
+	      .long   dummy_handler           /* PendSV Handler               */
+	      .long   dummy_handler           /* SysTick Handler              */
+
+	      /* External Interrupts */
+	      .long   dummy_handler
+	      .long   gpio_handler           /* GPIO even handler */
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   gpio_handler           /* GPIO odd handler */
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+
+	      	      .section .text
+
+	/////////////////////////////////////////////////////////////////////////////
+	//
+	// Reset handler
+  	// The CPU will start executing here after a reset	//
+	/////////////////////////////////////////////////////////////////////////////
+
+	//.include "polling_reset_handler.s"
+	//.include "interrupt_reset_handler.s"
+	
+.globl _reset
+.type _reset, %function
+.thumb_func
+_reset:
+
+	b . // Do nothing
+
+.thumb_func
+gpio_handler:
+	b .
+
+	
+.thumb_func
+dummy_handler:
+	b .
+//.include "base_addresses.s"
+cmu_base_addr:
+	.long CMU_BASE
+
+gpio_pa_base:
+	.long GPIO_PA_BASE
+
+gpio_pc_base:
+	.long GPIO_PC_BASE
+
+gpio_base:
+	.long GPIO_BASE
+
+iser0:
+	.long ISER0
+
+scr:
+	.long SCR
+
+
