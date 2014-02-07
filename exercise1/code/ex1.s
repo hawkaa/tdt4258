37e3cd22193262dc8cd6bcac95ba4836677c54d4
Sindre Magnussen <sindrma@gmail.com>
2014-02-07 22:57:12 +0100

    Final code. All good.

--- exercise1/code/ex1.s
+++ exercise1/code/ex1.s
@@ -1,6 +1,15 @@
-	.syntax unified
-	.include "efm32gg.s"
-	.section .vectors
+.syntax unified
+.include "efm32gg.s"
+
+	/////////////////////////////////////////////////////////////////////////////
+	//
+  	// Exception vector table
+  	// This table contains addresses for all exception handlers
+	//
+	/////////////////////////////////////////////////////////////////////////////
+	
+        .section .vectors
+	
 	      .long   stack_top               /* Top of Stack                 */
 	      .long   _reset                  /* Reset Handler                */
 	      .long   dummy_handler           /* NMI Handler                  */
@@ -16,11 +25,11 @@
 	      .long   dummy_handler           /* Debug Monitor Handler        */
 	      .long   dummy_handler           /* Reserved                     */
 	      .long   dummy_handler           /* PendSV Handler               */
-	      .long   systick_handler		      /* SysTick Handler              */
+	      .long   systick_handler         /* SysTick Handler              */
 
 	      /* External Interrupts */
 	      .long   dummy_handler
-	      .long   gpio_handler           /* GPIO even handler */
+	      .long   gpio_handler            /* GPIO even handler */
 	      .long   dummy_handler
 	      .long   dummy_handler
 	      .long   dummy_handler
@@ -30,7 +39,7 @@
 	      .long   dummy_handler
 	      .long   dummy_handler
 	      .long   dummy_handler
-	      .long   gpio_handler           /* GPIO odd handler */
+	      .long   gpio_handler            /* GPIO odd handler */
 	      .long   dummy_handler
 	      .long   dummy_handler
 	      .long   dummy_handler
@@ -59,61 +68,58 @@
 	      .long   dummy_handler
 	      .long   dummy_handler
 
-	      	      .section .text
+	      .section .text
 
 	/////////////////////////////////////////////////////////////////////////////
 	//
 	// Reset handler
-  	// The CPU will start executing here after a reset	//
+  	// The CPU will start executing here after a reset
+	//
 	/////////////////////////////////////////////////////////////////////////////
 
-	//.include "polling_reset_handler.s"
-	//.include "interrupt_reset_handler.s"
-	
-.globl _reset
-.type _reset, %function
+.globl  _reset
+.type   _reset, %function
 .thumb_func
 _reset:
-
-	// load CMU base adress
+	// Load CMU base address
 	ldr r1, cmu_base_addr
 	
 	// Enable GPIO-clk
-	// load current value of HFPERCLK ENABLE
+	// Load current value of HFPERCLK ENABLE
 	ldr r2, [r1, #CMU_HFPERCLKEN0]
 
-	// set bit for GPIO-clk
+	// Set bit for GPIO-clk
 	mov r3, #1
 	lsl r3, r3 , #CMU_HFPERCLKEN0_GPIO 
 	orr r2, r2, r3
 
-	// store new value
+	// Store new value
 	str r2, [r1,  #CMU_HFPERCLKEN0]
-	
-	mov r2, 0x00000000
-	str r2, [r1, #CMU_LFCLKSEL]	
+
+	// EM3 enable (requires EM2 code enabled)
+	//mov r2, 0x00000000
+	//str r2, [r1, #CMU_LFCLKSEL]	
 
 	ldr r1, gpio_pa_base
-
+	
+	// Set high drive strength
 	mov r2, 0x2
 	str r2, [r1, #GPIO_CTRL]
 	
+	// Set pins 8-15 to output
 	mov r2, 0x55555555
 	str r2, [r1, #GPIO_MODEH]
 	
 	ldr r3, gpio_pc_base
 	
+	// Set pins 0-7 to input
 	mov r2, 0x33333333
 	str r2, [r3, #GPIO_MODEL]
 
+	//Enable internal pull-up
 	mov r2, 0xff
 	str r2, [r3, #GPIO_DOUT]
-	
-	// Set one light
-	//mov r3, 0x22
-	//strb r3, [r1, #GPIO_DOUT]
 
-	
 	//
 	// INTERRUPT
 	//
@@ -125,39 +131,49 @@ _reset:
 	mov r2, 0x22222222
 	str r2, [r1, #GPIO_EXTIPSELL]
 
-	// We want on button down
+	// We want interrupt on button down
 	mov r2, 0xff
 	str r2, [r1, #GPIO_EXTIFALL]
-	str r2, [r1, #GPIO_EXTIRISE]
+	//str r2, [r1, #GPIO_EXTIRISE]
 
 	// Enable interrupt generation
 	mov r2, 0xff
 	str r2, [r1, #GPIO_IEN]
 	str r2, [r1, #GPIO_IFC]
-	
-	//systick
-	mov r2, 0x7 
-	ldr r1, =0xe000e010 //enable timer
-	strb r2, [r1] //configure systick
-	
-	mov r2, 0x00ffffff  //longest possible interval
-	ldr r1, =0xe000e014 //load reset val
-	strb r2, [r1] //set reload value
-	
-	mov r5, 0x1
-	
-	//
-	ldr r1, iser0
-	ldr r2, =0x802
-	str r2, [r1]
-	
-	ldr r1, scr
-	mov r2, #6
-	str r2, [r1]
-	
-	wfi
 
-	// EM4 Test
+	// Enable interrupt-handling
+        ldr r1, iser0
+        ldr r2, =0x802
+        str r2, [r1]
+
+	//
+	// SysTick
+	//
+
+
+	// Enable SysTick
+	mov r2, 0x7 
+	ldr r1, st_ctrl       // Enable timer
+	strb r2, [r1] 	      // Configure systick
+	
+	// Set count-down timer
+	ldr r2, =0x00ffffff  // Longest possible interval
+	ldr r1, st_load      //	Set interval-time
+	str r2, [r1] 
+
+	// Enable deep sleep (EM2) and automatic return from interrupt
+        //ldr r1, scr
+        //mov r2, #6
+        //str r2, [r1]         
+	
+	// Sleep
+	//wfi
+
+	// Jump to the sleeping loop.
+	b sleep_loop
+
+	// EM4 init. WFI is no longer required. Write 2, 3, 2, 3, 2, 3, 2, 3, 2 to EMU-
+	// control to enable EM4 (no easy way to wake up found).
 	//ldr r1, emu_base
 	//mov r2, #2
 	//lsl r2, r2, #2
@@ -173,59 +189,74 @@ _reset:
 	//str r3, [r1, #EMU_CTRL]
 	//str r2, [r1, #EMU_CTRL]
 
+
+// Goes to sleep. On wakeup it will return to sleep. Required for EM1. 
+sleep_loop:
+	wfi
+	b sleep_loop
+	    
 	
+	/////////////////////////////////////////////////////////////////////////////
+	//
+  	// GPIO handler
+  	// The CPU will jump here when there is a GPIO interrupt
+	//
+	/////////////////////////////////////////////////////////////////////////////
 
-
+// Simply read the copy values from buttons to LED's
 .thumb_func
-gpio_handler:
-	//ldr r1, gpio_pa_base
-	//ldr r2, gpio_pc_base
+gpio_handler:  
+	ldr r1, gpio_pa_base
+	ldr r2, gpio_pc_base
 	ldr r4, gpio_base
-	
-	// change some led lights
-	//mov r3, 0x33
-	//strb r3, [r1, #GPIO_DOUT]
-	//ldrb r3, [r2, #GPIO_DIN]
-	//strb r3, [r1, #GPIO_DOUT]	
-	
-	// small subroutine-program
-	//bl prog
-	b execute
 
-continue:		
+	ldrb r3, [r2, #GPIO_DIN]
+	strb r3, [r1, #GPIO_DOUT]
+	
 	// reset the interrupt
 	mov r3, 0xff
 	str r3, [r4, #GPIO_IFC]
 
-
+	// Exit interrupt-handler
 	bx lr 
-
-.thumb_func
-prog:
-	push {lr}
-	ldr r1, gpio_pa_base 
-	ldr r2, gpio_pc_base
-	ldr r3
-
-execute:
-	// change some led lights
-	//ldrb r3, [r2, #GPIO_DIN]
-	//strb r3, [r1, #GPIO_DOUT]
 	
-	b continue
+	/////////////////////////////////////////////////////////////////////////////
+	//
+	// SysTick Handler
+	// The CPU will jump here when there is a SysTick interrupt
+	//
+	////////////////////////////////////////////////////////////////////////////
 
+
+// Called when SysTick-interrupt occurs (timer tick). Shift lights right.
 .thumb_func
 systick_handler:
+	// Read button value
 	ldr r1, gpio_pa_base
-	strb r5, [r1, #GPIO_DOUT]
-	lsl r5, r5, #1
-
-	bx lr
+	ldr r5, [r1, #GPIO_DIN]
+	// Or with 16 bit number ending with eight 1's
+	mov r6, 0x00ff
+	orr r5, r5, r6
 	
+	// Check if we have reached the last LED in the sequence
+	mov r7, 0x8000
+	and r7, r5, r7
+	mov r6, 0x0
+	cmp r7, r6 
+	// If we have not reached the last LED, add a zero to the ninth bit, else continue.
+	bgt lol
+	ldr r6, =0xff7f
+	and r5, r5, r6
+lol:
+	lsl r5, r5, #1
+	str r5, [r1, #GPIO_DOUT]
+	// Exit SysTick-handler
+	bx lr
+
 .thumb_func
-dummy_handler:
-	b .
-//.include "base_addresses.s"
+dummy_handler:  
+        b .  // do nothing
+
 cmu_base_addr:
 	.long CMU_BASE
 
@@ -246,4 +277,10 @@ scr:
 
 emu_base:
 	.long EMU_BASE
+
+st_ctrl:
+	.long 0xe000e010
+
+st_load:
+	.long 0xe000e014
 
