cf4e027bd4f54da32d7ce88f160810b71db1e8fc
Kim Rune Solstad <kimrs@dmlab10.(none)>
2014-02-23 16:45:59 +0100

    piano spill

--- exercise2/code/interrupt_handlers.c
+++ exercise2/code/interrupt_handlers.c
@@ -5,12 +5,42 @@
 /* local headers */
 #include "efm32gg.h"
 
-
+int pitch = 0;
 static void
 gpio_handler(void)
 {
-//	*GPIO_PA_DOUT = *GPIO_PC_DIN << 8;
+	*GPIO_PA_DOUT = *GPIO_PC_DIN << 8;
 	*GPIO_IFC = 0xff;
+	switch(*GPIO_PC_DIN)
+	{
+		case 0xfe:
+			pitch =  100;
+			break;
+		case 0xfd:
+			pitch = 120;
+			break;
+		case 0xfb:
+			pitch = 140;
+			break;
+		case 0xf7:
+			pitch = 160;
+			break;
+		case 0xef:
+			pitch = 180;
+			break;
+		case 0xdf:
+			pitch = 200;
+			break;
+		case 0xbf:
+			pitch = 220;
+			break;
+		case 0x7f:
+			pitch = 240;
+			break;		
+		default:
+			pitch = 0;
+			break;
+	}
 }
 
 
@@ -26,11 +56,13 @@ TIMER1_IRQHandler(void)
 	 */
 
 	++i;
-	i = i % 291;
+	i = i % pitch;
+
+	if(i == 200)
+		*GPIO_PA_DOUT = *GPIO_PC_DIN << 8;
 
 
-
-	*DAC0_CH0DATA = *DAC0_CH1DATA = (i * 1024) / 291;
+	*DAC0_CH0DATA = *DAC0_CH1DATA = (i * 1024) / pitch;
 
 	*TIMER1_IFC = 1;
 }
