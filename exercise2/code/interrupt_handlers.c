9951adea92e450f6775a5f0e6e455b8409c1bc1f
Kim Rune Solstad <kimrs@dmlab10.(none)>
2014-02-23 15:47:40 +0100

    modified:   dac.c

--- exercise2/code/interrupt_handlers.c
+++ exercise2/code/interrupt_handlers.c
@@ -9,11 +9,13 @@
 static void
 gpio_handler(void)
 {
-	*GPIO_PA_DOUT = *GPIO_PC_DIN << 8;
+//	*GPIO_PA_DOUT = *GPIO_PC_DIN << 8;
 	*GPIO_IFC = 0xff;
 }
 
-static int i = 0;
+static int count = 0;
+static int direction = 1;
+static int digital = 0;
 void __attribute__ ((interrupt))
 TIMER1_IRQHandler(void)
 {
@@ -22,16 +24,19 @@ TIMER1_IRQHandler(void)
 	 * Feed new samples to the DAC. Remember to clear the pending
 	 * interrupt by writing 1 to TIMER1_IFC
 	 */
-
-	++i;
-	if(i % 100 == 0) {
-		*GPIO_PA_DOUT = ~(*GPIO_PA_DOUT);
+	if(count % 298)
+	{
+		*DAC0_CH0CTRL = digital;
+		*DAC0_CH1CTRL = digital;
 	}
+
+	count++;
+	digital += direction;
+	
 //	*GPIO_PA_DOUT = *GPIO_PC_DIN << 8;
-
-	//*GPIO_PA_DOUT = 0xaaff;
-
-	*TIMER1_IFC = 1;
+	*GPIO_PA_DOUT = *GPIO_PC_DIN << 8;
+	
+//	*TIMER1_IFC = 1;
 	//TIMER1_IF = 1;
 	//*GPIO_PA_DOUT  = 0xfafa;
 }
