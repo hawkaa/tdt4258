9e073e96c0091bddea69b8207ae1bcae3d91e92f
Håkon Åmdal <hakon@aamdal.com>
2014-02-27 11:25:55 +0100

    la til litt variable

--- exercise2/code/sampler.c
+++ exercise2/code/sampler.c
@@ -3,12 +3,19 @@
  * we can save alot of data here.
  */
 
+static int mode = 0;
+static int tick = 0;
+static int note_counter = 0;
+
+
 /*
  * Sets sampler mode
  */
 void
 set_mode(int mode) {
-	
+	mode = 0;
+	tick = 0;
+	node_counter = 0;
 }
 
 /*
