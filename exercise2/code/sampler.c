d4d5271996cc045526caf540429d1d83f0232db0
Håkon Åmdal <hakon@aamdal.com>
2014-02-27 11:24:56 +0100

    La til set_mode

--- exercise2/code/sampler.c
+++ exercise2/code/sampler.c
@@ -3,6 +3,13 @@
  * we can save alot of data here.
  */
 
+/*
+ * Sets sampler mode
+ */
+void
+set_mode(int mode) {
+	
+}
 
 /*
  * Will return a value between 0 an 2048
--- exercise2/code/sampler.h
+++ exercise2/code/sampler.h
@@ -1,6 +1,11 @@
 #ifndef SAMPLER_H_
 #define SAMPLER_H_
 
+/* set sampler mode */
+extern void
+set_mode(int mode);
+
+/* get sample */
 extern int
 get_sample();
 
