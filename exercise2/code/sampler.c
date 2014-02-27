d007529e7ce0ab53ee63923435547266a032aa38
Håkon Åmdal <hakon@aamdal.com>
2014-02-27 11:17:05 +0100

    lager sample

---
+++ exercise2/code/sampler.c
@@ -0,0 +1,14 @@
+
+/*
+ * we can save alot of data here.
+ */
+
+
+/*
+ * Will return a value between 0 an 2048
+ */
+int
+get_sample() {
+	return 0;
+}
+
---
+++ exercise2/code/sampler.h
@@ -0,0 +1,7 @@
+#ifndef SAMPLER_H_
+#define SAMPLER_H_
+
+extern int
+get_sample();
+
+#endif
