ab55f1f8849cea9f75d050556c06f6b7c54e9e1f
Håkon Åmdal <hakon@aamdal.com>
2014-02-27 18:01:22 +0100

    nye

--- exercise2/sampler/imp.c
+++ exercise2/sampler/imp.c
@@ -1,72 +1,144 @@
 /* note G4, length 4 */
 sample[0][0].hz = 392.000000;
-sample[0][0].ms = 386.250000;
+sample[0][0].ms = 300.416667;
 
 /* pause */
 sample[0][1].hz = 0.000000;
-sample[0][1].ms = 42.916667;
+sample[0][1].ms = 128.750000;
 
 /* note G4, length 4 */
 sample[0][1].hz = 392.000000;
-sample[0][1].ms = 386.250000;
+sample[0][1].ms = 300.416667;
 
 /* pause */
 sample[0][2].hz = 0.000000;
-sample[0][2].ms = 42.916667;
+sample[0][2].ms = 128.750000;
 
 /* note G4, length 4 */
 sample[0][2].hz = 392.000000;
-sample[0][2].ms = 386.250000;
+sample[0][2].ms = 300.416667;
 
 /* pause */
 sample[0][3].hz = 0.000000;
-sample[0][3].ms = 42.916667;
+sample[0][3].ms = 128.750000;
 
-/* note E4, length 6 */
-sample[0][3].hz = 329.630000;
-sample[0][3].ms = 257.500000;
+/* note D#4/Eb4, length 6 */
+sample[0][3].hz = 311.130000;
+sample[0][3].ms = 200.277778;
 
 /* pause */
 sample[0][4].hz = 0.000000;
-sample[0][4].ms = 28.611111;
+sample[0][4].ms = 85.833333;
 
 /* note A4, length 16 */
 sample[0][4].hz = 440.000000;
-sample[0][4].ms = 96.562500;
+sample[0][4].ms = 75.104167;
 
 /* pause */
 sample[0][5].hz = 0.000000;
-sample[0][5].ms = 10.729167;
+sample[0][5].ms = 32.187500;
 
 /* note G4, length 4 */
 sample[0][5].hz = 392.000000;
-sample[0][5].ms = 386.250000;
+sample[0][5].ms = 300.416667;
 
 /* pause */
 sample[0][6].hz = 0.000000;
-sample[0][6].ms = 42.916667;
+sample[0][6].ms = 128.750000;
 
-/* note E4, length 6 */
-sample[0][6].hz = 329.630000;
-sample[0][6].ms = 257.500000;
+/* note D#4/Eb4, length 6 */
+sample[0][6].hz = 311.130000;
+sample[0][6].ms = 200.277778;
 
 /* pause */
 sample[0][7].hz = 0.000000;
-sample[0][7].ms = 28.611111;
+sample[0][7].ms = 85.833333;
 
 /* note A4, length 16 */
 sample[0][7].hz = 440.000000;
-sample[0][7].ms = 96.562500;
+sample[0][7].ms = 75.104167;
 
 /* pause */
 sample[0][8].hz = 0.000000;
-sample[0][8].ms = 10.729167;
+sample[0][8].ms = 32.187500;
 
 /* note G4, length 2 */
 sample[0][8].hz = 392.000000;
-sample[0][8].ms = 772.500000;
+sample[0][8].ms = 600.833333;
 
 /* pause */
 sample[0][9].hz = 0.000000;
-sample[0][9].ms = 85.833333;
+sample[0][9].ms = 257.500000;
+
+/* note D5, length 4 */
+sample[0][9].hz = 587.330000;
+sample[0][9].ms = 300.416667;
+
+/* pause */
+sample[0][10].hz = 0.000000;
+sample[0][10].ms = 128.750000;
+
+/* note D5, length 4 */
+sample[0][10].hz = 587.330000;
+sample[0][10].ms = 300.416667;
+
+/* pause */
+sample[0][11].hz = 0.000000;
+sample[0][11].ms = 128.750000;
+
+/* note D5, length 4 */
+sample[0][11].hz = 587.330000;
+sample[0][11].ms = 300.416667;
+
+/* pause */
+sample[0][12].hz = 0.000000;
+sample[0][12].ms = 128.750000;
+
+/* note D#5/Eb5, length 6 */
+sample[0][12].hz = 622.250000;
+sample[0][12].ms = 200.277778;
+
+/* pause */
+sample[0][13].hz = 0.000000;
+sample[0][13].ms = 85.833333;
+
+/* note A#4/Bb4, length 16 */
+sample[0][13].hz = 466.160000;
+sample[0][13].ms = 75.104167;
+
+/* pause */
+sample[0][14].hz = 0.000000;
+sample[0][14].ms = 32.187500;
+
+/* note F4, length 4 */
+sample[0][14].hz = 349.230000;
+sample[0][14].ms = 300.416667;
+
+/* pause */
+sample[0][15].hz = 0.000000;
+sample[0][15].ms = 128.750000;
+
+/* note D#4/Eb4, length 6 */
+sample[0][15].hz = 311.130000;
+sample[0][15].ms = 200.277778;
+
+/* pause */
+sample[0][16].hz = 0.000000;
+sample[0][16].ms = 85.833333;
+
+/* note A4, length 16 */
+sample[0][16].hz = 440.000000;
+sample[0][16].ms = 75.104167;
+
+/* pause */
+sample[0][17].hz = 0.000000;
+sample[0][17].ms = 32.187500;
+
+/* note G4, length 2 */
+sample[0][17].hz = 392.000000;
+sample[0][17].ms = 600.833333;
+
+/* pause */
+sample[0][18].hz = 0.000000;
+sample[0][18].ms = 257.500000;
 
