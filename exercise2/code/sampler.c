cea4464afe3b2cc32bec6e43e6c9b15debfb363f
Kim Rune Solstad <kimrs@dmlab04.(none)>
2014-03-01 11:47:05 +0100

    flere spor funker kanskje

--- exercise2/code/sampler.c
+++ exercise2/code/sampler.c
@@ -1,11 +1,16 @@
 /* local include */
 #include "sampler.h"
 
+#define MAX_TRACKS 8
 /*
  * we can save alot of data here.
  */
-static sample_t sample[8][20]; 
-static int sample_sizes[8];
+static sample_t sample[MAX_TRACKS][20]; 
+static int sample_sizes[MAX_TRACKS];
+static int current_sample_length[MAX_TRACKS];
+static int current_sample_index[MAX_TRACKS];
+static sample_t current_sample[8];
+
 static int mode = 0;
 static int note_counter = 0;
 static int threshold = 0;
@@ -13,9 +18,170 @@ static int i = 0;
 
 static float pull_counter = 0.0;
 static int sample_index = 0;
-static int current_sample_length = 0;
 
 static const int FREQUENCY = 47945;
+
+/*
+ * Initialize
+ */
+void
+sampler_init() {
+	/* note G4, length 4 */
+	sample[0][0].hz = 392.000000;
+	sample[0][0].ms = 300.416667;
+	
+	/* pause */
+	sample[0][1].hz = 0.000000;
+	sample[0][1].ms = 128.750000;
+	
+	/* note G4, length 4 */
+	sample[0][1].hz = 392.000000;
+	sample[0][1].ms = 300.416667;
+	
+	/* pause */
+	sample[0][2].hz = 0.000000;
+	sample[0][2].ms = 128.750000;
+	
+	/* note G4, length 4 */
+	sample[0][2].hz = 392.000000;
+	sample[0][2].ms = 300.416667;
+	
+	/* pause */
+	sample[0][3].hz = 0.000000;
+	sample[0][3].ms = 128.750000;
+	
+	/* note D#4/Eb4, length 6 */
+	sample[0][3].hz = 311.130000;
+	sample[0][3].ms = 200.277778;
+	
+	/* pause */
+	sample[0][4].hz = 0.000000;
+	sample[0][4].ms = 85.833333;
+	
+	/* note A4, length 16 */
+	sample[0][4].hz = 440.000000;
+	sample[0][4].ms = 75.104167;
+	
+	/* pause */
+	sample[0][5].hz = 0.000000;
+	sample[0][5].ms = 32.187500;
+	
+	/* note G4, length 4 */
+	sample[0][5].hz = 392.000000;
+	sample[0][5].ms = 300.416667;
+	
+	/* pause */
+	sample[0][6].hz = 0.000000;
+	sample[0][6].ms = 128.750000;
+	
+	/* note D#4/Eb4, length 6 */
+	sample[0][6].hz = 311.130000;
+	sample[0][6].ms = 200.277778;
+	
+	/* pause */
+	sample[0][7].hz = 0.000000;
+	sample[0][7].ms = 85.833333;
+	
+	/* note A4, length 16 */
+	sample[0][7].hz = 440.000000;
+	sample[0][7].ms = 75.104167;
+	
+	/* pause */
+	sample[0][8].hz = 0.000000;
+	sample[0][8].ms = 32.187500;
+	
+	/* note G4, length 2 */
+	sample[0][8].hz = 392.000000;
+	sample[0][8].ms = 600.833333;
+	
+	/* pause */
+	sample[0][9].hz = 0.000000;
+	sample[0][9].ms = 257.500000;
+	
+	/* note D5, length 4 */
+	sample[0][9].hz = 587.330000;
+	sample[0][9].ms = 300.416667;
+	
+	/* pause */
+	sample[0][10].hz = 0.000000;
+	sample[0][10].ms = 128.750000;
+	
+	/* note D5, length 4 */
+	sample[0][10].hz = 587.330000;
+	sample[0][10].ms = 300.416667;
+	
+	/* pause */
+	sample[0][11].hz = 0.000000;
+	sample[0][11].ms = 128.750000;
+	
+	/* note D5, length 4 */
+	sample[0][11].hz = 587.330000;
+	sample[0][11].ms = 300.416667;
+	
+	/* pause */
+	sample[0][12].hz = 0.000000;
+	sample[0][12].ms = 128.750000;
+	
+	/* note D#5/Eb5, length 6 */
+	sample[0][12].hz = 622.250000;
+	sample[0][12].ms = 200.277778;
+	
+	/* pause */
+	sample[0][13].hz = 0.000000;
+	sample[0][13].ms = 85.833333;
+	
+	/* note A#4/Bb4, length 16 */
+	sample[0][13].hz = 466.160000;
+	sample[0][13].ms = 75.104167;
+	
+	/* pause */
+	sample[0][14].hz = 0.000000;
+	sample[0][14].ms = 32.187500;
+	
+	/* note F4, length 4 */
+	sample[0][14].hz = 349.230000;
+	sample[0][14].ms = 300.416667;
+	
+	/* pause */
+	sample[0][15].hz = 0.000000;
+	sample[0][15].ms = 128.750000;
+	
+	/* note D#4/Eb4, length 6 */
+	sample[0][15].hz = 311.130000;
+	sample[0][15].ms = 200.277778;
+	
+	/* pause */
+	sample[0][16].hz = 0.000000;
+	sample[0][16].ms = 85.833333;
+	
+	/* note A4, length 16 */
+	sample[0][16].hz = 440.000000;
+	sample[0][16].ms = 75.104167;
+	
+	/* pause */
+	sample[0][17].hz = 0.000000;
+	sample[0][17].ms = 32.187500;
+	
+	/* note G4, length 2 */
+	sample[0][17].hz = 392.000000;
+	sample[0][17].ms = 600.833333;
+	
+	/* pause */
+	sample[0][18].hz = 0.000000;
+	sample[0][18].ms = 257.500000;
+	sample_sizes[0] = 18;
+	
+	for(int j = 1; j < MAX_TRACKS; j++)
+	{
+		sample_sizes[j] = 0;
+	}
+	
+	for(int j = 0; j < MAX_TRACKS; j++)
+	{
+		current_sample_length[j] = 0;
+		current_sample_index[j] = 0;
+	}
+}
 
 static int
 get_sawtooth_signal(int j, int t, int max)
@@ -66,87 +232,6 @@ set_hz(float hz)
 }
 
 /*
- * Initialize
- */
-void
-sampler_init() {
-/* note G4, length 4 */
-sample[0][0].hz = 392.000000;
-sample[0][0].ms = 386.250000;
-
-/* pause */
-sample[0][1].hz = 0.000000;
-sample[0][1].ms = 42.916667;
-
-/* note G4, length 4 */
-sample[0][1].hz = 392.000000;
-sample[0][1].ms = 386.250000;
-
-/* pause */
-sample[0][2].hz = 0.000000;
-sample[0][2].ms = 42.916667;
-
-/* note G4, length 4 */
-sample[0][2].hz = 392.000000;
-sample[0][2].ms = 386.250000;
-
-/* pause */
-sample[0][3].hz = 0.000000;
-sample[0][3].ms = 42.916667;
-
-/* note E4, length 6 */
-sample[0][3].hz = 329.630000;
-sample[0][3].ms = 257.500000;
-
-/* pause */
-sample[0][4].hz = 0.000000;
-sample[0][4].ms = 28.611111;
-
-/* note A4, length 16 */
-sample[0][4].hz = 440.000000;
-sample[0][4].ms = 96.562500;
-
-/* pause */
-sample[0][5].hz = 0.000000;
-sample[0][5].ms = 10.729167;
-
-/* note G4, length 4 */
-sample[0][5].hz = 392.000000;
-sample[0][5].ms = 386.250000;
-
-/* pause */
-sample[0][6].hz = 0.000000;
-sample[0][6].ms = 42.916667;
-
-/* note E4, length 6 */
-sample[0][6].hz = 329.630000;
-sample[0][6].ms = 257.500000;
-
-/* pause */
-sample[0][7].hz = 0.000000;
-sample[0][7].ms = 28.611111;
-
-/* note A4, length 16 */
-sample[0][7].hz = 440.000000;
-sample[0][7].ms = 96.562500;
-
-/* pause */
-sample[0][8].hz = 0.000000;
-sample[0][8].ms = 10.729167;
-
-/* note G4, length 2 */
-sample[0][8].hz = 392.000000;
-sample[0][8].ms = 772.500000;
-
-/* pause */
-sample[0][9].hz = 0.000000;
-sample[0][9].ms = 85.833333;
-
-sample_sizes[0] = 10;
-//	set_hz(10);
-}
-
-/*
  * Sets sampler mode
  */
 void
@@ -187,6 +272,25 @@ sampler_set_mode(int mode) {
 	}
 }
 
+/*
+ * Decreases the remaing time in the sample. If there is none left,
+ * the next sample is set to current.
+ */
+void
+update_track(int track)
+{
+	pull_counter = 0.0;
+	/*holds the remaining amount of millisecounds in the sample */
+	current_sample_length[track]--;
+	/* true when the track needs to change sample */
+	if(current_sample_length[track] <= 0.0 && sample_sizes[track] >= current_sample_index[track])
+	{
+		current_sample_length[track] = sample[track][sample_index].ms;
+		set_hz(sample[track][sample_index].hz);
+		/* the next sample is selected */
+		++sample_index;
+	}
+}
 
 /*
  * Will return a value between 0 an 2048
@@ -197,20 +301,8 @@ sampler_get()
 	pull_counter += 1.0;
 	/* true when one ms has passed */
 	if(pull_counter > (FREQUENCY / 1000))
-	{
-		pull_counter = 0.0;
-		/*holds the remaining amount of millisecounds in the sample */
-		current_sample_length--;
-		/* true when the track needs to change sample */
-		if(current_sample_length <= 0.0 && sample_sizes[0] >= sample_index)
-		{
-
-			current_sample_length = sample[0][sample_index].ms;
-			set_hz(sample[0][sample_index].hz);
-			/* the next sample is selected */
-			++sample_index;
-		}
-	}
+//		for(int j = 0; j < MAX_TRACKS; j++)
+			update_track(0);
 
 	if(threshold ==0)
 		return 0;
