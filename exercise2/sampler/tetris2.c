23fb96f1ec924085a9fa17de46dde79af2c6cbe6
Sindre Magnussen <sindrma@gmail.com>
2014-03-02 13:19:07 +0100

    Finished low energy timer.

--- exercise2/sampler/tetris2.c
+++ exercise2/sampler/tetris2.c
@@ -4,178 +4,338 @@
 
 /* note E5, length 4 */
 sample[2][0].hz = 659.250000;
-sample[2][0].ms = 350.000000;
+sample[2][0].ms = 175.000000;
 
 /* pause */
 sample[2][1].hz = 0.000000;
-sample[2][1].ms = 150.000000;
+sample[2][1].ms = 75.000000;
 
 /* note B4, length 8 */
 sample[2][1].hz = 493.880000;
-sample[2][1].ms = 175.000000;
+sample[2][1].ms = 87.500000;
 
 /* pause */
 sample[2][2].hz = 0.000000;
-sample[2][2].ms = 75.000000;
+sample[2][2].ms = 37.500000;
 
 /* note C5, length 8 */
 sample[2][2].hz = 523.250000;
-sample[2][2].ms = 175.000000;
+sample[2][2].ms = 87.500000;
 
 /* pause */
 sample[2][3].hz = 0.000000;
-sample[2][3].ms = 75.000000;
+sample[2][3].ms = 37.500000;
 
 /* note D5, length 8 */
 sample[2][3].hz = 587.330000;
-sample[2][3].ms = 175.000000;
+sample[2][3].ms = 87.500000;
 
 /* pause */
 sample[2][4].hz = 0.000000;
-sample[2][4].ms = 75.000000;
+sample[2][4].ms = 37.500000;
 
 /* note 0, length 16 */
 sample[2][4].hz = 0.000000;
-sample[2][4].ms = 87.500000;
+sample[2][4].ms = 43.750000;
 
 /* pause */
 sample[2][5].hz = 0.000000;
-sample[2][5].ms = 37.500000;
+sample[2][5].ms = 18.750000;
 
 /* note 0, length 16 */
 sample[2][5].hz = 0.000000;
-sample[2][5].ms = 87.500000;
+sample[2][5].ms = 43.750000;
 
 /* pause */
 sample[2][6].hz = 0.000000;
-sample[2][6].ms = 37.500000;
+sample[2][6].ms = 18.750000;
 
 /* note C5, length 8 */
 sample[2][6].hz = 523.250000;
-sample[2][6].ms = 175.000000;
+sample[2][6].ms = 87.500000;
 
 /* pause */
 sample[2][7].hz = 0.000000;
-sample[2][7].ms = 75.000000;
+sample[2][7].ms = 37.500000;
 
 /* note B4, length 8 */
 sample[2][7].hz = 493.880000;
-sample[2][7].ms = 175.000000;
+sample[2][7].ms = 87.500000;
 
 /* pause */
 sample[2][8].hz = 0.000000;
-sample[2][8].ms = 75.000000;
+sample[2][8].ms = 37.500000;
 
 /* note A4, length 4 */
 sample[2][8].hz = 440.000000;
-sample[2][8].ms = 350.000000;
+sample[2][8].ms = 175.000000;
 
 /* pause */
 sample[2][9].hz = 0.000000;
-sample[2][9].ms = 150.000000;
+sample[2][9].ms = 75.000000;
 
 /* note A4, length 8 */
 sample[2][9].hz = 440.000000;
-sample[2][9].ms = 175.000000;
+sample[2][9].ms = 87.500000;
 
 /* pause */
 sample[2][10].hz = 0.000000;
-sample[2][10].ms = 75.000000;
+sample[2][10].ms = 37.500000;
 
 /* note C5, length 8 */
 sample[2][10].hz = 523.250000;
-sample[2][10].ms = 175.000000;
+sample[2][10].ms = 87.500000;
 
 /* pause */
 sample[2][11].hz = 0.000000;
-sample[2][11].ms = 75.000000;
+sample[2][11].ms = 37.500000;
 
 /* note E5, length 4 */
 sample[2][11].hz = 659.250000;
-sample[2][11].ms = 350.000000;
+sample[2][11].ms = 175.000000;
 
 /* pause */
 sample[2][12].hz = 0.000000;
-sample[2][12].ms = 150.000000;
+sample[2][12].ms = 75.000000;
 
 /* note D5, length 8 */
 sample[2][12].hz = 587.330000;
-sample[2][12].ms = 175.000000;
+sample[2][12].ms = 87.500000;
 
 /* pause */
 sample[2][13].hz = 0.000000;
-sample[2][13].ms = 75.000000;
+sample[2][13].ms = 37.500000;
 
 /* note C5, length 8 */
 sample[2][13].hz = 523.250000;
-sample[2][13].ms = 175.000000;
+sample[2][13].ms = 87.500000;
 
 /* pause */
 sample[2][14].hz = 0.000000;
-sample[2][14].ms = 75.000000;
+sample[2][14].ms = 37.500000;
 
 /* note B4, length 8 */
 sample[2][14].hz = 493.880000;
-sample[2][14].ms = 175.000000;
+sample[2][14].ms = 87.500000;
 
 /* pause */
 sample[2][15].hz = 0.000000;
-sample[2][15].ms = 75.000000;
+sample[2][15].ms = 37.500000;
 
 /* note B4, length 4 */
 sample[2][15].hz = 493.880000;
-sample[2][15].ms = 350.000000;
+sample[2][15].ms = 175.000000;
 
 /* pause */
 sample[2][16].hz = 0.000000;
-sample[2][16].ms = 150.000000;
+sample[2][16].ms = 75.000000;
 
 /* note C5, length 8 */
 sample[2][16].hz = 523.250000;
-sample[2][16].ms = 175.000000;
+sample[2][16].ms = 87.500000;
 
 /* pause */
 sample[2][17].hz = 0.000000;
-sample[2][17].ms = 75.000000;
+sample[2][17].ms = 37.500000;
 
 /* note D5, length 4 */
 sample[2][17].hz = 587.330000;
-sample[2][17].ms = 350.000000;
+sample[2][17].ms = 175.000000;
 
 /* pause */
 sample[2][18].hz = 0.000000;
-sample[2][18].ms = 150.000000;
+sample[2][18].ms = 75.000000;
 
 /* note E5, length 4 */
 sample[2][18].hz = 659.250000;
-sample[2][18].ms = 350.000000;
+sample[2][18].ms = 175.000000;
 
 /* pause */
 sample[2][19].hz = 0.000000;
-sample[2][19].ms = 150.000000;
+sample[2][19].ms = 75.000000;
 
 /* note C5, length 4 */
 sample[2][19].hz = 523.250000;
-sample[2][19].ms = 350.000000;
+sample[2][19].ms = 175.000000;
 
 /* pause */
 sample[2][20].hz = 0.000000;
-sample[2][20].ms = 150.000000;
+sample[2][20].ms = 75.000000;
 
 /* note A4, length 4 */
 sample[2][20].hz = 440.000000;
-sample[2][20].ms = 350.000000;
+sample[2][20].ms = 175.000000;
 
 /* pause */
 sample[2][21].hz = 0.000000;
-sample[2][21].ms = 150.000000;
+sample[2][21].ms = 75.000000;
 
 /* note A4, length 2 */
 sample[2][21].hz = 440.000000;
-sample[2][21].ms = 700.000000;
+sample[2][21].ms = 350.000000;
 
 /* pause */
 sample[2][22].hz = 0.000000;
-sample[2][22].ms = 300.000000;
+sample[2][22].ms = 150.000000;
 
-sample_sizes[2] = 23;
+/* note 0, length 8 */
+sample[2][22].hz = 0.000000;
+sample[2][22].ms = 87.500000;
+
+/* pause */
+sample[2][23].hz = 0.000000;
+sample[2][23].ms = 37.500000;
+
+/* note D5, length 4 */
+sample[2][23].hz = 587.330000;
+sample[2][23].ms = 175.000000;
+
+/* pause */
+sample[2][24].hz = 0.000000;
+sample[2][24].ms = 75.000000;
+
+/* note F5, length 8 */
+sample[2][24].hz = 698.460000;
+sample[2][24].ms = 87.500000;
+
+/* pause */
+sample[2][25].hz = 0.000000;
+sample[2][25].ms = 37.500000;
+
+/* note A5, length 4 */
+sample[2][25].hz = 880.000000;
+sample[2][25].ms = 175.000000;
+
+/* pause */
+sample[2][26].hz = 0.000000;
+sample[2][26].ms = 75.000000;
+
+/* note G5, length 8 */
+sample[2][26].hz = 783.990000;
+sample[2][26].ms = 87.500000;
+
+/* pause */
+sample[2][27].hz = 0.000000;
+sample[2][27].ms = 37.500000;
+
+/* note F5, length 8 */
+sample[2][27].hz = 698.460000;
+sample[2][27].ms = 87.500000;
+
+/* pause */
+sample[2][28].hz = 0.000000;
+sample[2][28].ms = 37.500000;
+
+/* note E5, length 4 */
+sample[2][28].hz = 659.250000;
+sample[2][28].ms = 175.000000;
+
+/* pause */
+sample[2][29].hz = 0.000000;
+sample[2][29].ms = 75.000000;
+
+/* note E5, length 8 */
+sample[2][29].hz = 659.250000;
+sample[2][29].ms = 87.500000;
+
+/* pause */
+sample[2][30].hz = 0.000000;
+sample[2][30].ms = 37.500000;
+
+/* note C5, length 8 */
+sample[2][30].hz = 523.250000;
+sample[2][30].ms = 87.500000;
+
+/* pause */
+sample[2][31].hz = 0.000000;
+sample[2][31].ms = 37.500000;
+
+/* note E5, length 4 */
+sample[2][31].hz = 659.250000;
+sample[2][31].ms = 175.000000;
+
+/* pause */
+sample[2][32].hz = 0.000000;
+sample[2][32].ms = 75.000000;
+
+/* note D5, length 8 */
+sample[2][32].hz = 587.330000;
+sample[2][32].ms = 87.500000;
+
+/* pause */
+sample[2][33].hz = 0.000000;
+sample[2][33].ms = 37.500000;
+
+/* note C5, length 8 */
+sample[2][33].hz = 523.250000;
+sample[2][33].ms = 87.500000;
+
+/* pause */
+sample[2][34].hz = 0.000000;
+sample[2][34].ms = 37.500000;
+
+/* note B4, length 4 */
+sample[2][34].hz = 493.880000;
+sample[2][34].ms = 175.000000;
+
+/* pause */
+sample[2][35].hz = 0.000000;
+sample[2][35].ms = 75.000000;
+
+/* note B4, length 8 */
+sample[2][35].hz = 493.880000;
+sample[2][35].ms = 87.500000;
+
+/* pause */
+sample[2][36].hz = 0.000000;
+sample[2][36].ms = 37.500000;
+
+/* note C5, length 8 */
+sample[2][36].hz = 523.250000;
+sample[2][36].ms = 87.500000;
+
+/* pause */
+sample[2][37].hz = 0.000000;
+sample[2][37].ms = 37.500000;
+
+/* note D5, length 4 */
+sample[2][37].hz = 587.330000;
+sample[2][37].ms = 175.000000;
+
+/* pause */
+sample[2][38].hz = 0.000000;
+sample[2][38].ms = 75.000000;
+
+/* note E5, length 4 */
+sample[2][38].hz = 659.250000;
+sample[2][38].ms = 175.000000;
+
+/* pause */
+sample[2][39].hz = 0.000000;
+sample[2][39].ms = 75.000000;
+
+/* note C5, length 4 */
+sample[2][39].hz = 523.250000;
+sample[2][39].ms = 175.000000;
+
+/* pause */
+sample[2][40].hz = 0.000000;
+sample[2][40].ms = 75.000000;
+
+/* note A4, length 4 */
+sample[2][40].hz = 440.000000;
+sample[2][40].ms = 175.000000;
+
+/* pause */
+sample[2][41].hz = 0.000000;
+sample[2][41].ms = 75.000000;
+
+/* note A4, length 4 */
+sample[2][41].hz = 440.000000;
+sample[2][41].ms = 175.000000;
+
+/* pause */
+sample[2][42].hz = 0.000000;
+sample[2][42].ms = 75.000000;
+
+sample_sizes[2] = 43;
