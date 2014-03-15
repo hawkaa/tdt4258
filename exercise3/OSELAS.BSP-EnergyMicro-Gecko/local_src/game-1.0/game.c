90a5cf60d4b2bec0dc04f7585118aaf0ad2c5f89
Håkon Åmdal <hakon@aamdal.com>
2014-03-15 13:09:56 +0100

    klar til bygg med makefile

--- exercise3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/game.c
+++ exercise3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/game.c
@@ -4,6 +4,8 @@
 int main(int argc, char *argv[])
 {
 	printf("Hello World, I'm game!\n");
+	printf("Sindrepupp, , whyfort are thou called sindrepupp sindrepupp!!!!\n");
+	printf("Brennevænn\n");
 
 	exit(EXIT_SUCCESS);
 }
