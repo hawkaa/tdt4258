afd05458b3784a8a0aeaf0bf97cb562aeb41f44f
Håkon Åmdal <hakon@aamdal.com>
2014-03-15 13:15:17 +0100

    fjernet ting

--- exercise3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/game.c
+++ exercise3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/game.c
@@ -6,6 +6,7 @@ int main(int argc, char *argv[])
 	printf("Hello World, I'm game!\n");
 	printf("Sindrepupp, , whyfort are thou called sindrepupp sindrepupp!!!!\n");
 	printf("Brennevænn\n");
+	printf("Får jeg noen error her?");
 
 	exit(EXIT_SUCCESS);
 }
