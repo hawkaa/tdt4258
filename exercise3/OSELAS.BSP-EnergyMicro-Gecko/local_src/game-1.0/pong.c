4941549de9ab0289d9e8e4218c27806252895a72
KimRuneSolstad <kimrunesolstad89@gmail.com>
2014-03-23 17:56:58 +0100

    game further developed

--- exercise3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/pong.c
+++ exercise3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/pong.c
@@ -1,6 +1,7 @@
 /* global includes  */
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
 /* local includes */
 #include "pong.h"
@@ -12,12 +13,13 @@
 #define PLAYER_HEIGHT 50
 #define PLAYER_SPEED 1
 #define BALL_SIZE 5
-#define MARGIN 10
+#define MARGIN_HORIZONTAL 10
+#define MARGIN_VERTICAL 10
 
 static screen_elem player_left, player_right, ball;
 static int game_over = 1;
 
-static void reset_ball_pos(void)
+static void reset_ball(void)
 {
 	ball.dx = 0;
 	ball.dy = 0;
@@ -28,16 +30,16 @@ static void reset_ball_pos(void)
 void
 pong_init(void)
 {
-	player_left.x = MARGIN;
-	player_left.y = MARGIN;
+	player_left.x = MARGIN_HORIZONTAL;
+	player_left.y = MARGIN_VERTICAL;
 	player_left.height = PLAYER_HEIGHT;
 	player_left.width = PLAYER_WIDTH;
 	player_left.c = 0xffff;
 	player_left.dx = 0;
 	player_left.dy = 0;
 	
-	player_right.x = SCREEN_WIDTH - (MARGIN + PLAYER_WIDTH);	
-	player_right.y = MARGIN;
+	player_right.x = SCREEN_WIDTH - (MARGIN_HORIZONTAL + PLAYER_WIDTH);	
+	player_right.y = MARGIN_VERTICAL;
 	player_right.height = PLAYER_HEIGHT;
 	player_right.width = PLAYER_WIDTH;
 	player_right.c = 0xffff;
@@ -47,7 +49,7 @@ pong_init(void)
 	ball.height = BALL_SIZE;
 	ball.width = BALL_SIZE;
 	ball.c = 0xff00;
-	reset_ball_pos();
+	reset_ball();
 }
 
 static void 
@@ -58,18 +60,18 @@ move_player(screen_elem* player)
 		return;
 
 	/* is on top */
-	if (player->y <= 0 && player->dy < 0)
+	if (player->y <= MARGIN_VERTICAL && player->dy < 0)
 		return;
 		
-	if(player->y >= SCREEN_HEIGHT - PLAYER_HEIGHT && player->dy > 0)
+	if(player->y >= (SCREEN_HEIGHT - MARGIN_VERTICAL) - PLAYER_HEIGHT && player->dy > 0)
 		return;
 	
 	/* everything good */
 	screen_elem old_player = *player;
 	if (player->dy > 0)
 	{
-		player->y = (player->y + player->dy > SCREEN_HEIGHT - PLAYER_HEIGHT ?
-					SCREEN_HEIGHT - PLAYER_HEIGHT : player->y + player->dy);
+		player->y = (player->y + MARGIN_VERTICAL +  player->dy > SCREEN_HEIGHT - MARGIN_VERTICAL - PLAYER_HEIGHT ?
+					SCREEN_HEIGHT - PLAYER_HEIGHT - MARGIN_VERTICAL: player->y + player->dy);
 	}
 	else if (player->dy < 0)
 	{
@@ -84,10 +86,12 @@ move_player(screen_elem* player)
 static void
 change_direction_if_intersection(screen_elem *ball, screen_elem *player)
 {
+	float angle;	
 	/* if ball intersects with player */
 	if(	ball->y <= player->y + PLAYER_HEIGHT &&
 		ball->y >= player->y)
-		ball->dx = -ball->dx;	
+		angle = asin(PLAYER_WIDTH / ( (ball->y + BALL_SIZE / 2) + (player->y + PLAYER_HEIGHT/2)));
+	
 	return;
 }
 
@@ -103,6 +107,7 @@ move_ball(screen_elem *ball)
 	{
 		ball->dx = 0;
 		ball->dy = 0;
+		game_over = 1;
 		return;
 	}		
 	
@@ -114,20 +119,20 @@ move_ball(screen_elem *ball)
 	ball->y += ball->dy;
 
 	/* if ball is within empty area */
-	if( ball->x >= MARGIN + PLAYER_WIDTH ||
-		ball->x <= SCREEN_WIDTH - (MARGIN + PLAYER_WIDTH))
+	if( ball->x >= MARGIN_HORIZONTAL + PLAYER_WIDTH ||
+		ball->x <= SCREEN_WIDTH - (MARGIN_HORIZONTAL + PLAYER_WIDTH))
 		return;	
 	
 	/* ball should now be within one of the player areas */
 	
 	/* if ball is within left player area */
-	if( ball->x <= MARGIN + PLAYER_WIDTH && ball->x > MARGIN)
+	if( ball->x <= MARGIN_HORIZONTAL + PLAYER_WIDTH && ball->x > MARGIN_HORIZONTAL)
 	{
 		change_direction_if_intersection(ball, &player_left);
 		return;
 	}
 	/* if ball is within right player area */
-	if( ball->x >= SCREEN_WIDTH - (MARGIN + PLAYER_WIDTH) && ball->x < SCREEN_WIDTH - MARGIN)
+	if( ball->x >= SCREEN_WIDTH - (MARGIN_HORIZONTAL + PLAYER_WIDTH) && ball->x < SCREEN_WIDTH - MARGIN_HORIZONTAL)
 	{
 		change_direction_if_intersection(ball, &player_right);	
 		return;
@@ -137,6 +142,11 @@ move_ball(screen_elem *ball)
 void 
 timer_tick(void)
 {
+	if(game_over)
+	{
+		game_over = 0;
+		reset_ball();
+	}
 	move_player(&player_left);
 	move_player(&player_right);
 	move_ball(&ball);	
